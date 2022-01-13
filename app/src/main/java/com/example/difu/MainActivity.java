7f2f43433b3c31cc47aaa5aabfeab4afa3232164
Gee Hunter <gh@localhost.localdomain>
2022-01-13 01:57:17 -0500

    authentication using firebase complete

--- app/src/main/java/com/example/difu/MainActivity.java
+++ app/src/main/java/com/example/difu/MainActivity.java
@@ -1,14 +1,112 @@
 package com.example.difu;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Patterns;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class MainActivity extends AppCompatActivity  implements View.OnClickListener{
+
+    private TextView register;
+    private EditText etEmail, etPassword;
+    private Button btnLogin;
+
+    private FirebaseAuth mAuth;
+    private ProgressBar progressBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        register = (TextView) findViewById(R.id.tvBackLogin);
+        register.setOnClickListener(this);
+
+        btnLogin = (Button)findViewById(R.id.btnLogin);
+        btnLogin.setOnClickListener(this);
+
+        etEmail = (EditText) findViewById(R.id.etEmail);
+        etPassword= (EditText) findViewById(R.id.etPassword);
+
+        progressBar = (ProgressBar) findViewById(R.id.progressBar);
+
+        mAuth = FirebaseAuth.getInstance();
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.tvBackLogin:
+                startActivity(new Intent(this,RegisterUser.class));
+                break;
+            case R.id.btnLogin:
+                userLogin();
+                break;
+        }
+
+    }
+
+    private void userLogin() {
+        String email = etEmail.getText().toString().trim();
+        String password = etPassword.getText().toString().trim();
+
+        if (email.isEmpty()) {
+            etEmail.setError("Email  required !");
+            etEmail.requestFocus();
+            return;
+        }
+        if(!Patterns.EMAIL_ADDRESS.matcher(email).matches()){
+            etEmail.setError("Enter a valid email");
+            etEmail.requestFocus();
+                 return;
+        }
+        if (password.isEmpty()) {
+            etPassword.setError("Password required !");
+            etPassword.requestFocus();
+            return;
+        }
+        if (password.length()<6){
+            etPassword.setError("Enter a Minimum of Six characters");
+            etPassword.requestFocus();
+            return;
+        }
+        progressBar.setVisibility(View.VISIBLE);
+
+        mAuth.signInWithEmailAndPassword(email,password).addOnCompleteListener
+                (new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+              if(task.isSuccessful()){
+                  //authenticating email
+                  FirebaseUser user =FirebaseAuth.getInstance().getCurrentUser();
+                  if(user.isEmailVerified()){
+                      //redirect to user profile
+                      startActivity(new Intent(MainActivity.this, Home.class));
+                  }else {
+                      user.sendEmailVerification();
+                      Toast.makeText(MainActivity.this, "Check your email to verify account",
+                              Toast.LENGTH_LONG).show();
+                  }
+
+
+              } else {
+                  Toast.makeText(MainActivity.this, "Login Unsuccessful! Try Again", Toast.LENGTH_LONG).show();
+              }
+            }
+        });
     }
 }
--- app/src/main/java/com/example/difu/RegisterUser.java
+++ app/src/main/java/com/example/difu/RegisterUser.java
@@ -1,14 +1,136 @@
 package com.example.difu;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Patterns;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class RegisterUser extends AppCompatActivity {
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.FirebaseDatabase;
 
+
+public class RegisterUser extends AppCompatActivity  implements View.OnClickListener {
+    private TextView logoText,registerUser, tvBackLogin;
+    private EditText ptFullName, ptAge, ptEmailReg, pwReg;
+    private ProgressBar progressBar;
+
+    private FirebaseAuth mAuth;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register_user);
+        mAuth = FirebaseAuth.getInstance();
+
+        logoText = (TextView) findViewById(R.id.logoText);
+        logoText.setOnClickListener(this);
+
+        tvBackLogin = (TextView)findViewById(R.id.tvBackLogin);
+        tvBackLogin.setOnClickListener(this);
+
+        registerUser = (Button)findViewById(R.id.btnRegister);
+        registerUser.setOnClickListener(this);
+
+        ptFullName = (EditText) findViewById(R.id.etEmail);
+        ptAge = (EditText) findViewById(R.id.ptAge);
+        ptEmailReg = (EditText) findViewById(R.id.ptEmailReg);
+        pwReg = (EditText) findViewById(R.id.pwReg);
+
+        progressBar = (ProgressBar) findViewById(R.id.progressBar);
+
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.logoText:
+                startActivity(new Intent(this,MainActivity.class));
+                break;
+            case R.id.tvBackLogin:
+                startActivity(new Intent(this,MainActivity.class));
+            case R.id.btnRegister:
+                registerUser();
+                break;
+        }
+
+    }
+
+    private void registerUser() {
+        String email= ptEmailReg.getText().toString().trim();
+        String password= pwReg.getText().toString().trim();
+        String fullname= ptFullName.getText().toString().trim();
+        String age= ptAge.getText().toString().trim();
+
+        if (fullname.isEmpty()){
+            ptFullName.setError("Full name required !");
+            ptFullName.requestFocus();
+            return;
+        }
+        if (age.isEmpty()){
+            ptAge.setError("Age required !");
+            ptAge.requestFocus();
+            return;
+        }
+        if (password.isEmpty()){
+            pwReg.setError("Password required !");
+            pwReg.requestFocus();
+            return;
+        }
+        if (email.isEmpty()){
+            ptEmailReg.setError("Email required !");
+            ptEmailReg.requestFocus();
+            return;
+        }
+        if(!Patterns.EMAIL_ADDRESS.matcher(email).matches()){
+            ptEmailReg.setError("Provide a valid address!");
+            ptEmailReg.requestFocus();
+            return;
+        }
+        if (password.length()<6){
+            pwReg.setError("Minimum length is 6 characters!");
+            pwReg.requestFocus();
+            return;
+        }
+        progressBar.setVisibility(View.VISIBLE);
+        mAuth.createUserWithEmailAndPassword(email, password).
+                addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if(task.isSuccessful()){
+                    User user = new User(fullname, age,email);
+                    FirebaseDatabase.getInstance().getReference("Users")
+                            .child(FirebaseAuth.getInstance().getCurrentUser().getUid())
+                            .setValue(user).addOnCompleteListener(new OnCompleteListener<Void>() {
+                        @Override
+                        public void onComplete(@NonNull Task<Void> task) {
+                            if(task.isSuccessful()){
+                                Toast.makeText(RegisterUser.this, "Registration Successful",Toast.LENGTH_LONG).show();
+                                progressBar.setVisibility(View.GONE);
+
+                                //REDIRECT TO LOGIN
+
+
+                            }else
+                                Toast.makeText(RegisterUser.this,"Registration failed! Try again.",Toast.LENGTH_LONG).show();
+                            progressBar.setVisibility(View.GONE);
+                        }
+                    });
+
+                }else
+                    Toast.makeText(RegisterUser.this,"Registration failed! Try again.",Toast.LENGTH_LONG).show();
+                progressBar.setVisibility(View.GONE);
+            }
+        });
     }
 }
