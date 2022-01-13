c6ea9384450681d7b88ddf22071b99574f1580ae
Gee Hunter <gh@localhost.localdomain>
2022-01-13 10:17:36 -0500

    added logout feature

--- app/src/main/java/com/example/difu/Home.java
+++ app/src/main/java/com/example/difu/Home.java
@@ -2,13 +2,33 @@ package com.example.difu;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class Home extends AppCompatActivity {
+
+    private ImageView logout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        logout= (ImageView) findViewById(R.id.logout);
+        logout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FirebaseAuth.getInstance().signOut();
+                Toast.makeText(Home.this, "You Logged Out!",
+                        Toast.LENGTH_SHORT).show();
+                startActivity(new Intent(Home.this,MainActivity.class));
+
+            }
+        });
     }
 }
